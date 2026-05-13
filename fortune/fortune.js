7fd480a3564940b629d92edc48f29c33a41688ae
Desiree <155199304+DesireeBoudreaux@users.noreply.github.com>
2026-05-12 21:28:24 -0500

    Add files via upload

---
+++ fortune/fortune.js
@@ -0,0 +1,11 @@
+
+//fortune.js
+let numberOfChildren = 3;
+let partnerName = "Sponge Bob";
+let location= "Kansas City";
+let jobTitle = "Developer";
+let salary = "$85,000/yr";
+
+let fortune = "You will be a " + jobTitle + " in " + location + " and married to " + partnerName + " with " + numberOfChildren + " kids " + " with a salary of " + salary + ".";
+              
+console.log(fortune);
